4e98445a343de6d284460e9d3748d277bc373108
ash-gth <ashisharma9318@gmail.com>
2025-02-08 13:18:07 +0530

    Commit by Ashish

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className=' bg-slate-500 text-center text-7xl'>Welcome Pookies! </h1>
+      <h1 className=' bg-slate-500 text-center text-7xl text-orange-500'>Welcome Pookies! </h1>
       <p className=' bg-slate-400 text-center'>It's gonna be hard for you to work without any prior experience</p>
       <p className=' bg-slate-300 text-center'>That's why your Leader is there so feel free to call out.</p>
       <p className=' bg-slate-200 text-center'>Let's do our best and do something Innovative</p>
