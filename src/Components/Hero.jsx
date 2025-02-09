212a03604100cb2b5070a7f564fcbbc67a6fc329
ash-gth <ashisharma9318@gmail.com>
2025-02-09 09:58:42 +0530

    Hero section modifying

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,17 +1,26 @@
-import React, { useDebugValue } from "react"
-import './Hero.css'
+import React, { useDebugValue } from "react";
+import "./Hero.css";
 
-
-
-function Hero()
-{
-    return(
+function Hero() {
+  return (
     <>
-    <main>
-    <div className="Hero">
-    <p className="Heading">Empowering Communities through Shared Solar Investment: A Blockchain-Based Sustainable Platform
-    </p></div>
-    </main>
+      <main>
+        <div className="Hero">
+          <p className="Heading">
+            Empowering Communities through Shared Solar Investment: 
+            A Blockchain-Based Sustainable Platform
+          </p>
+          <br></br>
+          <div>
+            <p className="Heading2">
+              A community solar sharing platform that enables individuals and
+              organizations to invest in solar projects and gain digital
+              ownership through NFTs.
+            </p>
+          </div>
+        </div>
+      </main>
     </>
-   )
-} export default Hero
+  );
+}
+export default Hero;
