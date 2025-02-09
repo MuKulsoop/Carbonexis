7228ff6a21b08c3ece6941b10c19a8e4381abdb6
ash-gth <ashisharma9318@gmail.com>
2025-02-09 19:30:55 +0530

    Trade Section almost complete

--- src/Components/TradeSection.jsx
+++ src/Components/TradeSection.jsx
@@ -4,25 +4,33 @@ import "./TradeSection.css";
 function TradeSection() {
   return (
     <>
-      <div className="Trade">
-
-
-        <div className="Hero"><h3>Trade Carbon Credit</h3>
+      <div className="Trade">    
+        <div className="Heading-T">
+          <h3>Trade Carbon Credit</h3>
         </div>
-       
-          <div className="TradeSection">
-            <p>
-             <b> Unlock the Future of Carbon Credit Trading Join a marketplace
-              where your sustainable choices make real impact. Offset your
-              carbon footprint and connect with a community of innovators
-              driving change. Transform your green initiatives into meaningful
-              contributions. The future of sustainable trading awaits—
-              </b></p>
+
+        <div className="TCC">
+
+          <div 
+           className="TradeSection">
+             <p className="Description-T">
+              <b>
+                {" "}
+                Unlock the Future of Carbon Credit Trading Join a marketplace
+                where your sustainable choices make real impact. Offset your
+                carbon footprint and connect with a community of innovators
+                driving change. Transform your green initiatives into meaningful
+                contributions. The future of sustainable trading awaits—
+              </b>
+            </p>
             <img className="Image1" src="Trade.png" alt="" />
-          </div>
-       
+            </div>
 
-
+            <div>
+              <button className="Button-T">Trade Now </button>
+            </div>
+        
+        </div>
       </div>
     </>
   );
