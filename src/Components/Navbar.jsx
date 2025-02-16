2f7052d70f00d4615455d3eb162a62f71fefdffa
Mukul <varun.gupta8851401313@gmail.com>
2025-02-16 20:03:39 +0530

    Added the react router dom

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,36 +7,37 @@ function NavBar() {
       <nav>
         <img src="STrax.svg" alt="" />
 
-       <div className="allNavItems">
-       <ul className="NavItems">
-          <li className="NavItem">
-            <a href="#">Home</a>
-          </li>
-          <li className="NavItem">
-            <a href="#">Projects</a>
-          </li>
-          <li className="NavItem">
-            <a href="#">Donate</a>
-          </li>
-          <li className="NavItem">
-            <a href="#">Dashboard</a>
-          </li>
-          <li className="NavItem">
-            <a href="#">Blogs</a>
-          </li>
-        </ul>
-       </div>
+        <div className="allNavItems">
+          <ul className="NavItems">
+            <li className="NavItem">
+              <a href="#">Home</a>
+            </li>
+            <li className="NavItem">
+              <a href="#">Projects</a>
+            </li>
+            <li className="NavItem">
+              <a href="/marketplace">Marketplace</a>
+            </li>
+            <li className="NavItem">
+              <a href="/marketplace">Marketplace</a>
+            </li>
+            <li className="NavItem">
+              <a href="#">Blogs</a>
+            </li>
+          </ul>
+        </div>
 
         <div className="Buttons">
-        <div className="Button-Login">
-              <button ><h2 className="DescriptionOfButtonOfLogin">Login</h2> </button>
-              </div>
-          <div className="Button-SignUp">
-          <button ><h2 className="DescriptionOfButtonOfSignUp">SignUp</h2> </button>
+          <div className="Button-Login">
+            <button>
+              <h2 className="DescriptionOfButtonOfLogin">Login</h2>{" "}
+            </button>
           </div>
-          
-            
-          
+          <div className="Button-SignUp">
+            <button>
+              <h2 className="DescriptionOfButtonOfSignUp">SignUp</h2>{" "}
+            </button>
+          </div>
         </div>
       </nav>
     </>
