58d2efcdd2de9208301546d268a99f8a291dad4d
Mukul <varun.gupta8851401313@gmail.com>
2025-02-20 12:36:56 +0530

    Solving the card issue

--- src/Pages/NFTIntsa.jsx
+++ src/Pages/NFTIntsa.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card.jsx';
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
